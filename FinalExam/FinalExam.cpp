ed3db48bb1b00148a6ae3b786849f7131b1d677a
Student <Student@B101-06>
2022-06-01 08:42:04 +0300

    Add project files.

---
+++ FinalExam/FinalExam.cpp
@@ -0,0 +1,20 @@
+/** \file FinalExam.cpp
+ * \brief Manipulations with matrix
+ * \details This program used to manipulate with matrix (enter data, negate elements and show them) with functions through menu
+ * \author Ivan Kosiakov
+ * \version 1.0
+ * \date 2022-2022
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/** Function <code>main</code> used to execute all the code of the program
+ *  </br>
+ *  @return Returns 0 to successfully end the program
+ */
+int main()
+{
+    //Code is going to be up here
+}
