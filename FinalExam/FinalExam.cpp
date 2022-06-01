56ced8178acb0fa35fc451874245d6f727ee5533
Ivan Kosiakov <ivan0kosyakov@gmail.com>
2022-06-01 09:08:30 +0300

    1)c)1 Created "enterMatrix" function and filled first case in switch that allows to test this function.
    Fixed case "4" that didn't contain "break" before

--- FinalExam/FinalExam.cpp
+++ FinalExam/FinalExam.cpp
@@ -16,6 +16,7 @@ int const COLUMNS = 4;
 
 //Function prototypes
 int menuCall();
+void enterMatrix(double[][COLUMNS], int const);
 
 /** Function <code>main</code> used to execute all the code of the program
  *  </br>
@@ -29,9 +30,9 @@ int main()
 
     do {
         choice = menuCall();
-        switch (menuCall()) {
+        switch (choice) {
         case 1:
-            //Entering data
+            enterMatrix(matrix, ROWS);
             break;
 
         case 2:
@@ -44,10 +45,11 @@ int main()
 
         case 4:
             //No code needed
+            break;
 
         default:
             //This switch cannot contains something exept the numbers 1-4
-            assert(true);
+            assert(false);
         }
     } while (choice != 4);
 }
@@ -74,4 +76,27 @@ int menuCall() {
     } while (choice < 1 || choice > 4);
 
     return choice;
+}
+
+/** Function <code>enterMatrix</code> used to enter data in matrix
+ *  </br>
+ *  @param matrix is the array that contains doubles with dimensions 3X4 that is going to be filled in.
+ */
+void enterMatrix(double matrix[][COLUMNS], int const rows) {
+    double const MIN = -10.5, MAX = 7.5;
+    double userEnter;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < COLUMNS; j++) {
+
+            do {
+                cout << "Enter value for matrix[" << i << "][" << j << "] in range from " << MIN << " to " << MAX << ": " << endl;
+                cin >> userEnter;
+
+                if (userEnter < MIN || userEnter > MAX)
+                    cout << "You've entered value that out of range! Try again!" << endl;
+            } while (userEnter < MIN || userEnter > MAX);
+
+        }
+    }
 }
