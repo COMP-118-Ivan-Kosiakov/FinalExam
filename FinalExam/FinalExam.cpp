cbe17575e428dbc41db0bcf2a526c2e55f32cf0f
Ivan Kosiakov <ivan0kosyakov@gmail.com>
2022-06-01 08:56:29 +0300

    1)b) Created function for menu and switch for menu points

--- FinalExam/FinalExam.cpp
+++ FinalExam/FinalExam.cpp
@@ -7,11 +7,15 @@
  */
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
 //Global Variables
 int const COLUMNS = 4;
+
+//Function prototypes
+int menuCall();
 
 /** Function <code>main</code> used to execute all the code of the program
  *  </br>
@@ -21,5 +25,53 @@ int main()
 {
     int const ROWS = 3;
     double matrix[ROWS][COLUMNS];
+    int choice;
 
+    do {
+        choice = menuCall();
+        switch (menuCall()) {
+        case 1:
+            //Entering data
+            break;
+
+        case 2:
+            //Negation of all elements
+            break;
+
+        case 3:
+            //Showing data in matrix
+            break;
+
+        case 4:
+            //No code needed
+
+        default:
+            //This switch cannot contains something exept the numbers 1-4
+            assert(true);
+        }
+    } while (choice != 4);
+}
+
+/** Function <code>menuCall</code> used to create a menu and allows user to choice a point of the menu (1-4)
+ *  </br>
+ *  @return Returns choice of the user
+ */
+int menuCall() {
+    int choice;
+
+    do {
+        cout << "1) Enter data in matrix (range -10.5 to 7.5)" << endl
+            << "2) Negate all element (i.e. if a element is -5 => 5, or element 5 => -5)" << endl
+            << "3) Show data in matrix" << endl
+            << "4) Exit" << endl
+            << "Enter your choise: ";
+
+        cin >> choice;
+
+
+        if (choice < 1 || choice > 4)
+            cout << "You've entered an invalid value! Try again!" << endl;
+    } while (choice < 1 || choice > 4);
+
+    return choice;
 }
